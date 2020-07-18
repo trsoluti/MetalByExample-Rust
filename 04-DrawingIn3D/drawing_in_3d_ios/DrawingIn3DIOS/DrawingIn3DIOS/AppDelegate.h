ba0ca6301889839c6511f68c2e519c7e05c3845a
trsolutions <thomas_odell@trsolutions.biz>
2020-07-18 11:06:58 +0800

    Added base template for DrawingIn3DIOS

---
+++ 04-DrawingIn3D/drawing_in_3d_ios/DrawingIn3DIOS/DrawingIn3DIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DrawingIn3DIOS
+//
+//  Created by TR Solutions on 18/7/20.
+//  Copyright © 2020 TR Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 04-DrawingIn3D/drawing_in_3d_ios/DrawingIn3DIOS/DrawingIn3DIOS/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DrawingIn3DIOS
+//
+//  Created by TR Solutions on 18/7/20.
+//  Copyright © 2020 TR Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
